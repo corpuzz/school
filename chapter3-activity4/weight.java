e19371dc588c9f171557138fff8e24ac76f21997
corpuzz <corpuz.markjanlee@gmail.com>
2022-10-15 18:21:51 +0800

    activity4 done

--- chapter3-activity4/weight.java
+++ chapter3-activity4/weight.java
@@ -1,5 +1,17 @@
-
+import javax.swing.JOptionPane;
 public class weight {
     public static void main(String[] args) {
+        double kg = Double.parseDouble(JOptionPane.showInputDialog(null, "Enter your weight(in kg): "));
+        double lbs = kg * 2.2;
+
+        JOptionPane.showMessageDialog(
+            null, 
+            "Weight in kilograms: " + String.format("%.2f", kg ) + "kg" +
+            "\nWeight in pounds: " + String.format("%.2f", lbs ) + "lbs" ,
+            "Weight",
+            JOptionPane.INFORMATION_MESSAGE
+        );
+
+
     }
 }
