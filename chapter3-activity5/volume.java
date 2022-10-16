39cebe391833908006158db09946e76f642fac66
corpuzz <corpuz.markjanlee@gmail.com>
2022-10-16 21:16:50 +0800

    ch3-act5 done

--- chapter3-activity5/volume.java
+++ chapter3-activity5/volume.java
@@ -1,5 +1,21 @@
-
+import javax.swing.JOptionPane;
 public class volume {
     public static void main(String[] args) {
+        double mass = Double.parseDouble(JOptionPane.showInputDialog(
+            null, 
+            "Enter the mass (in grams): "
+        ));
+        double density = Double.parseDouble(JOptionPane.showInputDialog(
+                null,
+                "Enter the density: "
+        ));
+
+        double volume = mass / density;
+
+        JOptionPane.showMessageDialog(
+            null, 
+            "Volume: " + String.format("%.2f", volume) + "g/cm³", 
+            "Volume", 
+            JOptionPane.INFORMATION_MESSAGE);
     }
 }
