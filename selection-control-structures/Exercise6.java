c6ff2e3cdbe7de151bd66c08d2294cd69ef1c844
corpuzz <corpuz.markjanlee@gmail.com>
2022-12-02 13:05:33 +0800

    final solution

--- selection-control-structures/Exercise6.java
+++ selection-control-structures/Exercise6.java
@@ -20,26 +20,84 @@ public class Exercise6 {
         System.out.println("Enter length of side 3: ");
         int side3 = prompt.nextInt();
 
-       int longest = 0;
-       int shorter = 0;
-       // Calculate if it's a right triangle
-       // Determine the longest
-       if(side1 > side2 || side1 > side3) longest = side1;
-       else if(side2 > side1 || side2 > side3) longest = side2;
-       else longest = side3;
-       // Determine the 2 lower value
-       if(side1 < longest) shorter += side1;
-       if(side2 < longest) shorter += side2;
-       if(side3 < longest) shorter += side3;
-       // Do comparison on hypotenuse(longest)^2 == (base(shorter side)^2 + height(other shorter side)^2)
-       if(Math.pow(longest, 2) == Math.pow(shorter, 2)) {
+        /* =========== first solution ====================
+          int longest = 0;
+          int shorter = 0;
+
+          // Determine the longest
+          if(side1 > side2 && side1 > side3) longest = side1;
+          else if(side2 > side1 && side2 > side3) longest = side2;
+          else longest = side3;
+
+          // Determine the 2 lower value, square each and add them together
+          if(side1 < longest) shorter += Math.pow(side1, 2);
+          if(side2 < longest) shorter += Math.pow(side2, 2);
+          if(side3 < longest) shorter += Math.pow(side3, 2);
+
+          // use the Pythagorean formula: a^2 + b^2 = c^2
+          boolean result = Math.pow(longest, 2) == shorter; 
+          if(result) {
+          System.out.println("It is a right triangle");
+          } else {
+          System.out.println("Not a right triangle");
+          }
+         */
+
+       // ============= second solution ==============
+       // sort in ascending order (make it easier to plug into the the pythagorean formula)
+       // With help from: https://stackoverflow.com/a/37145746
+
+       int max = 0;
+       int med = 0;
+       int min = 0;
+       if (side1 > side2) {
+            if (side1 > side3) {
+                 max = side1;
+                 if (side2 > side3) {
+                      med = side2;
+                      min = side3;
+                 } else {
+                      med = side3;
+                      min = side2;
+                 }
+            } else {
+                 med = side1;
+                 if (side2 > side3) {
+                      max = side2;
+                      min = side1;
+                 } else {
+                      max = side3;
+                      min = side2;
+                 }
+            }
+       } else {
+            if (side2 > side3) {
+                 max = side2;
+                 if (side1 > side3) {
+                      med = side1;
+                      min = side3;
+                 } else {
+                      med = side3;
+                      min = side1;
+                 }
+            } else {
+                 med = side2;
+                 max = side3;
+                 min = side1;
+            }
+       }
+
+       // use the pythagorean theorem to determine if it is a right triangle
+       // a^2 + b^2 = c^2
+       boolean result = Math.pow(min, 2) + Math.pow(med, 2) == Math.pow(max, 2);
+
+       if (result) {
             System.out.println("It is a right triangle");
        } else {
             System.out.println("Not a right triangle");
        }
-       // If true:
-       // It is a right triangle
-   }
+       prompt.close();
+  }
 }
 
 
