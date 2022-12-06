2280a0be1beed379249dcc188b4346deaf50236c
corpuzz <corpuz.markjanlee@gmail.com>
2022-12-06 10:41:30 +0800

    nalpas

--- repetition-control-structures/Exercise8.java
+++ repetition-control-structures/Exercise8.java
@@ -2,8 +2,14 @@
  * Write a complete program to generate the multiplication table ranging from 1 – 10
  */
 
-
 public class Exercise8 {
     public static void main(String[] args) {
+        for(int row = 1; row <= 10; row++) {
+            for(int column = 1; column <= 10; column++) {
+                int value = row * column;
+                System.out.print(" " + value + "\t");
+            }
+            System.out.print("\n");
+        }
     }
 }
