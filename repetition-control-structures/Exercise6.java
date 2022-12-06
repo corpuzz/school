cb15c1129fe57690c3f059067ea9481f94ae3c5f
corpuzz <corpuz.markjanlee@gmail.com>
2022-12-06 16:33:52 +0800

    finished

--- repetition-control-structures/Exercise6.java
+++ repetition-control-structures/Exercise6.java
@@ -7,8 +7,47 @@
  * e. Output the sum of the squares of all the odd numbers between firstNum and * secondNum inclusive.
  * f. Output all the uppercase letters.
  */
+import java.util.Scanner;
+import java.lang.Math;
+
 
 public class Exercise6 {
     public static void main(String[] args) {
+        // a. 
+        Scanner prompt = new Scanner(System.in);
+        System.out.println("Enter an integer: (must be lower than next integer)");
+        int firstNum = prompt.nextInt();
+        System.out.println("Enter another integer: ");
+        int secondNum = prompt.nextInt();
+        while(true) {
+            if(firstNum > secondNum) {
+                System.out.println("First number must be less than second number.");
+                System.out.println("Enter an integer: (must be lower than next integer)");
+                firstNum = prompt.nextInt();
+                System.out.println("Enter another integer: ");
+                secondNum = prompt.nextInt(); 
+            }
+            break;
+        }
+
+        // b.
+        int even = 0;
+        int odd = 0;
+        for(int i = firstNum; i <= secondNum; i++) {
+            if(i % 2 == 0) {
+                even += i;
+            } else {
+                System.out.println(i);
+                odd += i;
+            }
+        }
+
+        // c.
+        System.out.println("c. Sum of all even: " + even);
+
+
+        // e. 
+        System.out.println("e. Sum of squared odd " + (int)(Math.pow(odd, 2)));
+        prompt.close();
     }
 }
