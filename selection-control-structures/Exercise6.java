90b581eb51e406a82e9f5b46ad6bd04ae155d77a
corpuzz <corpuz.markjanlee@gmail.com>
2022-12-06 09:46:24 +0800

    made the sort algo concise

--- selection-control-structures/Exercise6.java
+++ selection-control-structures/Exercise6.java
@@ -11,68 +11,45 @@ import java.lang.Math;
 
 public class Exercise6 {
     public static void main(String[] args) {
-        Scanner prompt = new Scanner(System.in);
-        // Prompt user for length
-        System.out.println("Enter length of side 1: ");
-        int side1 = prompt.nextInt();
-        System.out.println("Enter length of side 2: ");
-        int side2 = prompt.nextInt();
-        System.out.println("Enter length of side 3: ");
-        int side3 = prompt.nextInt();
+          Scanner prompt = new Scanner(System.in);
+          // Prompt user for length
+          System.out.println("Enter length of side 1: ");
+          int side1 = prompt.nextInt();
+          System.out.println("Enter length of side 2: ");
+          int side2 = prompt.nextInt();
+          System.out.println("Enter length of side 3: ");
+          int side3 = prompt.nextInt();
 
-       // Sort in ascending order (make it easier to plug into the the pythagorean formula)
-       int max = 0;
-       int med = 0;
-       int min = 0;
-       if (side1 > side2) {
-            if (side1 > side3) {
-                 max = side1;
-                 if (side2 > side3) {
-                      med = side2;
-                      min = side3;
-                 } else {
-                      med = side3;
-                      min = side2;
-                 }
-            } else {
-                 med = side1;
-                 if (side2 > side3) {
-                      max = side2;
-                      min = side1;
-                 } else {
-                      max = side3;
-                      min = side2;
-                 }
-            }
-       } else {
-            if (side2 > side3) {
-                 max = side2;
-                 if (side1 > side3) {
-                      med = side1;
-                      min = side3;
-                 } else {
-                      med = side3;
-                      min = side1;
-                 }
-            } else {
-                 med = side2;
-                 max = side3;
-                 min = side1;
-            }
-       }
+          // Sort in ascending order (make it easier to plug into the the pythagorean formula)
+          int max = 0;
+          int med = 0;
+          int min = 0;
+          if(side1 > side2 && side1 > side3) { 
+               max = side1;
+               med = side2 > side3 ? side2 : side3; 
+               min = side2 > side3 ? side3 : side2;
+          } else if(side2 > side1 && side2 > side3) {                
+               max = side2;
+               med = side1 > side3 ? side1 : side3;
+               min = side1 > side3 ? side3 : side1;
+          } else { 
+               max = side3;
+               med = side2 > side1 ? side2 : side1;
+               min = side2 > side1 ? side1 : side2;
+          }
 
-       // Use the pythagorean formula to determine if it is a right triangle
-       // a^2 + b^2 = c^2
-       boolean result = Math.pow(min, 2) + Math.pow(med, 2) == Math.pow(max, 2);
+          // Use the pythagorean formula to determine if it is a right triangle
+          // a^2 + b^2 = c^2
+          boolean result = Math.pow(min, 2) + Math.pow(med, 2) == Math.pow(max, 2);
 
-       // Print the results
-       if (result) {
-            System.out.println("It is a right triangle");
-       } else {
-            System.out.println("Not a right triangle");
-       }
-       // Close the scanner resource
-       prompt.close();
+          // Print the results
+          if (result) {
+               System.out.println("It is a right triangle");
+          } else {
+               System.out.println("Not a right triangle");
+          }
+          // Close the scanner resource
+          prompt.close();
   }
 }
 
