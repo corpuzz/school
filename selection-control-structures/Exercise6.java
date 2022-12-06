ca206296afdb283834e840bb7b53170b05366594
corpuzz <corpuz.markjanlee@gmail.com>
2022-12-06 09:07:58 +0800

    provided comments

--- selection-control-structures/Exercise6.java
+++ selection-control-structures/Exercise6.java
@@ -20,33 +20,7 @@ public class Exercise6 {
         System.out.println("Enter length of side 3: ");
         int side3 = prompt.nextInt();
 
-        /* =========== first solution ====================
-          int longest = 0;
-          int shorter = 0;
-
-          // Determine the longest
-          if(side1 > side2 && side1 > side3) longest = side1;
-          else if(side2 > side1 && side2 > side3) longest = side2;
-          else longest = side3;
-
-          // Determine the 2 lower value, square each and add them together
-          if(side1 < longest) shorter += Math.pow(side1, 2);
-          if(side2 < longest) shorter += Math.pow(side2, 2);
-          if(side3 < longest) shorter += Math.pow(side3, 2);
-
-          // use the Pythagorean formula: a^2 + b^2 = c^2
-          boolean result = Math.pow(longest, 2) == shorter; 
-          if(result) {
-          System.out.println("It is a right triangle");
-          } else {
-          System.out.println("Not a right triangle");
-          }
-         */
-
-       // ============= second solution ==============
-
-       // sort in ascending order (make it easier to plug into the the pythagorean formula)
-       // With help from: https://stackoverflow.com/a/37145746
+       // Sort in ascending order (make it easier to plug into the the pythagorean formula)
        int max = 0;
        int med = 0;
        int min = 0;
@@ -87,15 +61,17 @@ public class Exercise6 {
             }
        }
 
-       // use the pythagorean formula to determine if it is a right triangle
+       // Use the pythagorean formula to determine if it is a right triangle
        // a^2 + b^2 = c^2
        boolean result = Math.pow(min, 2) + Math.pow(med, 2) == Math.pow(max, 2);
 
+       // Print the results
        if (result) {
             System.out.println("It is a right triangle");
        } else {
             System.out.println("Not a right triangle");
        }
+       // Close the scanner resource
        prompt.close();
   }
 }
