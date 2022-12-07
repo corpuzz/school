155a6bf0d6c4c891bfe292a41e1e22863788d279
corpuzz <corpuz.markjanlee@gmail.com>
2022-12-07 13:47:49 +0800

    code refactor

--- selection-control-structures/Exercise9.java
+++ selection-control-structures/Exercise9.java
@@ -13,33 +13,54 @@ import java.util.Scanner;
 public class Exercise9 {
     public static void main(String[] args) {
         Scanner prompt = new Scanner(System.in);
-        // Prompt for the base price
-        System.out.println("Enter base price per square foot");
-        int base_price_per_sqft = prompt.nextInt();
 
-        // Prompt for the finished area of models in square feet
-        System.out.println("Please enter the finished area of the colonial model in square feet: ");
-        int area_of_colonial_in_sqft = prompt.nextInt();
-        System.out.println("Please enter the finished area of the split-entry model in square feet: ");
-        int area_of_split_entry_in_sqft = prompt.nextInt();
-        System.out.println("Please enter the finished area of the single-story model in square feet: ");
-        int area_of_single_story_in_sqft = prompt.nextInt();
+        double colonial_price, split_entry_price, single_story_price;
+        double colonial_area, split_entry_area, single_story_area;
 
-        // Print model prices
-        System.out.println("Price of a colonial model: $" + (base_price_per_sqft * area_of_colonial_in_sqft));
-        System.out.println("Price of a split-entry model: $" + (base_price_per_sqft * area_of_split_entry_in_sqft));
-        System.out.println("Price of a single-story model: $" + (base_price_per_sqft * area_of_single_story_in_sqft));
+        // Prompt the user for the base price and finished area for the colonial model
+        System.out.print("Enter the base price for the colonial model: ");
+        colonial_price = prompt.nextDouble();
+        System.out.print("Enter the finished area (in square feet) for the colonial model: ");
+        colonial_area = prompt.nextDouble();
 
-        // determine the lowest priced model
-        if(area_of_colonial_in_sqft < area_of_split_entry_in_sqft && area_of_colonial_in_sqft < area_of_split_entry_in_sqft) {
-            System.out.println("Lowest price is the colonial model at $" + area_of_colonial_in_sqft);
+        // Prompt the user for the base price and finished area for the split-entry
+        // model
+        System.out.print("Enter the base price for the split-entry model: ");
+        split_entry_price = prompt.nextDouble();
+        System.out.print("Enter the finished area (in square feet) for the split-entry model: ");
+        split_entry_area = prompt.nextDouble();
+
+        // Prompt the user the base price and finished area for the single-story model
+        System.out.print("Enter the base price for the single-story model: ");
+        single_story_price = prompt.nextDouble();
+        System.out.print("Enter the finished area (in square feet) for the single-story model: ");
+        single_story_area = prompt.nextDouble();
+
+        // Calculate the price per square foot for each model
+        double colonial_price_per_sqft = colonial_price / colonial_area;
+        double split_entry_price_per_sqft = split_entry_price / split_entry_area;
+        double single_story_per_sqft = single_story_price / single_story_area;
+
+        // Print the price per square foot for each model
+        System.out.println("Price per square foot for colonial model: $" + colonial_price_per_sqft);
+        System.out.println("Price per square foot for split-entry model: $" + split_entry_price_per_sqft);
+        System.out.println("Price per square foot for single-story model: $" + single_story_per_sqft);
+
+        // Determine the model with the lowest price per square foot
+        // Assume Colonial has the lowest price per square foot (similar with the Pythagoreas theorem)
+        String least_expensive_model = "colonial";
+        double lowest_price_per_sqft = colonial_price_per_sqft;
+        if (split_entry_price_per_sqft < lowest_price_per_sqft) {
+            least_expensive_model = "split-entry";
+            lowest_price_per_sqft = split_entry_price_per_sqft;
         }
-        else if(area_of_split_entry_in_sqft < area_of_colonial_in_sqft && area_of_split_entry_in_sqft < area_of_single_story_in_sqft) {
-            System.out.println("Lowest price is the split-entry model at $" + area_of_split_entry_in_sqft);
-        } else {
-            System.out.println("Lowest price is the single-story model at $" + area_of_single_story_in_sqft);
+        if (single_story_per_sqft < lowest_price_per_sqft) {
+            least_expensive_model = "single story";
+            lowest_price_per_sqft = single_story_per_sqft;
         }
-        
+
+        // Print the model with the lowest price per square foot
+        System.out.println("The least expensive model is the " + least_expensive_model + " model");
         prompt.close();
     }
 }
