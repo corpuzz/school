19c2f7935e8939e1371e37bbab33fc0cc71d1461
corpuzz <corpuz.markjanlee@gmail.com>
2022-12-17 03:38:11 +0800

    formatted code plus converted while selection menu to switch

--- finals-project/InventoryManagementSystem.java
+++ finals-project/InventoryManagementSystem.java
@@ -1,37 +1,41 @@
 import javax.swing.*;
 
 public class InventoryManagementSystem {
-
+    // Inventory with capacity of 100 products
     private static String[] products = new String[100];
     private static int[] quantities = new int[100];
     private static Double[] prices = new Double[100];
-    private static int numProducts = 0; // we'll use this to index through our product list
+    private static int numProducts = 0; // Index to map product to it's quantity and and  
 
     public static void main(String[] args) {
-        while (true) {
+        while(true) {
             // Show selection Menu
             int selection = showMainMenu();
 
-            if (selection == 0) {
-                addProduct();
+            switch(selection) {
+                case 0:
+                    addProduct();
+                    break;
+                case 1:
+                    viewInventory();
+                    break;
+                case 2:
+                    removeProduct();
+                    break;
+                case 3:
+                    // Exit program
+                    break;
+                default:
+                    // Handle invalid input
+                    break;
             }
-            else if (selection == 1) {
-                viewInventory();
-            }
-            else if (selection == 2) {
-                removeProduct();
-            }
-            // Exit the program
-            else if (selection == 3) {
+            if(selection == 3)
                 break;
-            }
         }
-
     }
 
     private static int showMainMenu() {
         String[] options = {"Add a new product", "View inventory", "Remove a product", "Exit"};
-
         int selection = JOptionPane.showOptionDialog(
                 null,
                 "Please select an option:",
@@ -42,18 +46,29 @@ public class InventoryManagementSystem {
                 options,
                 options[0]
         );
-
         return selection;
     }
 
     private static void addProduct() {
         // Get product name, quantity and price
-        String name = JOptionPane.showInputDialog(null, "Please enter the product name:");
-        String quantityString = JOptionPane.showInputDialog(null, "Please enter the product quantity:");
-        String priceString = JOptionPane.showInputDialog(null, "Please enter the product price:");
+        String name = JOptionPane.showInputDialog(
+            null, 
+            "Please enter the product name:", 
+            null, 
+            JOptionPane.INFORMATION_MESSAGE);
+
+        String quantityString = JOptionPane.showInputDialog(null, 
+            "Please enter the product quantity:", 
+            null, 
+            JOptionPane.QUESTION_MESSAGE);
+
+        String priceString = JOptionPane.showInputDialog(
+            null, 
+            "Please enter the product price:", 
+            null, 
+            JOptionPane.QUESTION_MESSAGE);
 
         try {
-            // Parse the quantity and price 
             int quantity = Integer.parseInt(quantityString);
             Double price = Double.parseDouble(priceString);
 
@@ -63,62 +78,87 @@ public class InventoryManagementSystem {
             prices[numProducts] = price;
             numProducts++;
 
-            JOptionPane.showMessageDialog(null, "Product added successfully!");
+            JOptionPane.showMessageDialog(
+                null, 
+                "Product added successfully!", 
+                null, 
+                JOptionPane.INFORMATION_MESSAGE);
         } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(null, "Invalid quantity or price. Please try again.");
+            JOptionPane.showMessageDialog(
+                null, 
+                "Invalid quantity or price. Please try again.",
+                
+                null, 
+                JOptionPane.WARNING_MESSAGE);
         }
     }
 
     private static void viewInventory() {
         // Add a label for the inventory
-        String table = "";
+        String inventory = "";
         if(products[0] == null) {
-         table = String.format("%-20s %-10s %-10s%n", "Product Name", "Quantity", "Price");
-         table += String.format("%nNo items found.");
+         inventory = String.format("%-20s %-20s %-20s%n", "Products", "Quantity", "Price");
+         inventory += String.format("%nNo items found.");
         } else {
-         table = String.format("%-20s %-10s %-10s%n", "Product Name", "Quantity", "Price");
+         inventory = String.format("%-20s %-22s %-24s%n", "Products", "Quantity", "Price");
         }
-
         // Add each product to the invetory
         for (int i = 0; i < numProducts; i++) {
             String name = products[i];
             int quantity = quantities[i];
             Double price = prices[i];
-            table += String.format("%-20s %-10d %-10.2f%n", name, quantity, price);
+            inventory += String.format("%-20s %-22d %-24.2f%n", name, quantity, price);
         }
         // Display the inventory
-        JOptionPane.showMessageDialog(null, table);
+        JOptionPane.showMessageDialog(null, inventory);
     }
+
     private static void removeProduct() {
         // Check if there are products to remove
         if(products[0] == null) {
-            JOptionPane.showMessageDialog(null, "Inventory is empty.");
+            JOptionPane.showMessageDialog(
+                null, 
+                "Inventory is empty.",
+                null,
+                JOptionPane.INFORMATION_MESSAGE);
             return;
         }
         // Get the product name from the user
-        String name = JOptionPane.showInputDialog(null, "Please enter the product name:");
+        String name = JOptionPane.showInputDialog(
+            null, 
+            "Please enter the product name:", 
+            null, 
+            JOptionPane.QUESTION_MESSAGE);
 
-        // Search for the product in the inventory
-        int index = -1;
-        for (int i = 0; i < numProducts; i++) {
+        // Search for the product 
+        int index = -1; // holds the index for the product to be removed
+        for (int i = 0; i < products.length; i++) {
             if (products[i].equals(name.toUpperCase())) {
                 index = i;
                 break;
             }
         }
 
-        // If the product was found, remove it from the inventory
-        // pop product at index i and rearrange indexes
+        // Remove product from the inventory
+        // (reassigning values of array)
         if (index != -1) {
             for (int i = index; i < numProducts - 1; i++) {
                 products[i] = products[i + 1];
                 quantities[i] = quantities[i + 1];
                 prices[i] = prices[i + 1];
             }
-            numProducts--;
-            JOptionPane.showMessageDialog(null, "Product removed successfully!");
+            numProducts--; // Decrement total number of products
+            JOptionPane.showMessageDialog(
+                null, 
+                "Product removed successfully!", 
+                null, 
+                JOptionPane.INFORMATION_MESSAGE);
         } else {
-            JOptionPane.showMessageDialog(null, "Product not found. Please try again.");
+            JOptionPane.showMessageDialog(
+                null, 
+                "Product not found. Please try again.",
+                null,
+                JOptionPane.WARNING_MESSAGE);
         }
     }
 }
