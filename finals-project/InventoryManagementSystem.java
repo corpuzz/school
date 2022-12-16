df74d143662858a71c86cd541202e4c7a87b5758
corpuzz <corpuz.markjanlee@gmail.com>
2022-12-17 02:53:43 +0800

    finals done

--- finals-project/InventoryManagementSystem.java
+++ finals-project/InventoryManagementSystem.java
@@ -1,58 +1,124 @@
-
-import javax.swing.JList;
-import javax.swing.JOptionPane;
-import java.util.ArrayList;
+import javax.swing.*;
 
 public class InventoryManagementSystem {
-        private static ArrayList<String> Products = new ArrayList<String>();
-        private static ArrayList<Double> Prices = new ArrayList<Double>();
-        private static ArrayList<Integer> Quantities = new ArrayList<Integer>();
+
+    private static String[] products = new String[100];
+    private static int[] quantities = new int[100];
+    private static Double[] prices = new Double[100];
+    private static int numProducts = 0; // we'll use this to index through our product list
+
     public static void main(String[] args) {
-        DisplayInventory();
+        while (true) {
+            // Show selection Menu
+            int selection = showMainMenu();
+
+            if (selection == 0) {
+                addProduct();
+            }
+            else if (selection == 1) {
+                viewInventory();
+            }
+            else if (selection == 2) {
+                removeProduct();
+            }
+            // Exit the program
+            else if (selection == 3) {
+                break;
+            }
+        }
+
     }
 
-    public static void DisplayInventory() {
-        if(Products.isEmpty()) {
-            AddToInventory();
-        } 
-        getInventory();
-    }
+    private static int showMainMenu() {
+        String[] options = {"Add a new product", "View inventory", "Remove a product", "Exit"};
 
-    // Getting the products has the responsibility of displaying the 
-    public static void getInventory() {
-        JList<String> JProducts = new JList<String>(convertToJlist(Products));
-        JOptionPane.showMessageDialog(null, JProducts, "INVENTORY", JOptionPane.PLAIN_MESSAGE);
-    }
-
-    // Adding to products is responsible for asking for the product, price and quantity
-    public static void AddToInventory() {
-        String product = JOptionPane.showInputDialog(
-            null, 
-            "There are no items found.\nAdd a product", 
-             "PRODUCT",
-            JOptionPane.OK_CANCEL_OPTION
+        int selection = JOptionPane.showOptionDialog(
+                null,
+                "Please select an option:",
+                "INVENTORY MANAGEMENT SYSTEM",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                options,
+                options[0]
         );
 
-        double price = Double.parseDouble(JOptionPane.showInputDialog(
-            null, 
-            "Enter the unit price:", 
-             "PRICE",
-            JOptionPane.OK_CANCEL_OPTION
-        ));
-
-        int quantity = Integer.parseInt(JOptionPane.showInputDialog(
-            null, 
-            "Enter quantity:", 
-             "QUANTITY",
-            JOptionPane.OK_CANCEL_OPTION
-        ));
-        Products.add(product);
-        Prices.add(price);
-        Quantities.add(quantity);
+        return selection;
     }
 
-    public static String[] convertToJlist(ArrayList<String> s) {
-        String[] something = {"ham", "egg"};
-        return something;
+    private static void addProduct() {
+        // Get product name, quantity and price
+        String name = JOptionPane.showInputDialog(null, "Please enter the product name:");
+        String quantityString = JOptionPane.showInputDialog(null, "Please enter the product quantity:");
+        String priceString = JOptionPane.showInputDialog(null, "Please enter the product price:");
+
+        try {
+            // Parse the quantity and price 
+            int quantity = Integer.parseInt(quantityString);
+            Double price = Double.parseDouble(priceString);
+
+            // Add the product to the inventory
+            products[numProducts] = name.toUpperCase();
+            quantities[numProducts] = quantity;
+            prices[numProducts] = price;
+            numProducts++;
+
+            JOptionPane.showMessageDialog(null, "Product added successfully!");
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Invalid quantity or price. Please try again.");
+        }
+    }
+
+    private static void viewInventory() {
+        // Add a label for the inventory
+        String table = "";
+        if(products[0] == null) {
+         table = String.format("%-20s %-10s %-10s%n", "Product Name", "Quantity", "Price");
+         table += String.format("%nNo items found.");
+        } else {
+         table = String.format("%-20s %-10s %-10s%n", "Product Name", "Quantity", "Price");
+        }
+
+        // Add each product to the invetory
+        for (int i = 0; i < numProducts; i++) {
+            String name = products[i];
+            int quantity = quantities[i];
+            Double price = prices[i];
+            table += String.format("%-20s %-10d %-10.2f%n", name, quantity, price);
+        }
+        // Display the inventory
+        JOptionPane.showMessageDialog(null, table);
+    }
+    private static void removeProduct() {
+        // Check if there are products to remove
+        if(products[0] == null) {
+            JOptionPane.showMessageDialog(null, "Inventory is empty.");
+            return;
+        }
+        // Get the product name from the user
+        String name = JOptionPane.showInputDialog(null, "Please enter the product name:");
+
+        // Search for the product in the inventory
+        int index = -1;
+        for (int i = 0; i < numProducts; i++) {
+            if (products[i].equals(name.toUpperCase())) {
+                index = i;
+                break;
+            }
+        }
+
+        // If the product was found, remove it from the inventory
+        // pop product at index i and rearrange indexes
+        if (index != -1) {
+            for (int i = index; i < numProducts - 1; i++) {
+                products[i] = products[i + 1];
+                quantities[i] = quantities[i + 1];
+                prices[i] = prices[i + 1];
+            }
+            numProducts--;
+            JOptionPane.showMessageDialog(null, "Product removed successfully!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Product not found. Please try again.");
+        }
     }
 }
